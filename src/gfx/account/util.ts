ae08645d72c2965e4e8f62a941f518072e4aea06
Ademola <83315082+0xcrust@users.noreply.github.com>
2025-04-08 13:39:52 +0100

    Run yarn lint

--- src/gfx/account/util.ts
+++ src/gfx/account/util.ts
@@ -65,7 +65,9 @@ export function generatePubKey({
 }
 
 function createWithSeed(fromPublicKey: PublicKey, seed: string, programId: PublicKey): PublicKey {
-  const buffer = Buffer.concat([fromPublicKey.toBuffer(), Buffer.from(seed), programId.toBuffer()].map((buf) => new Uint8Array(buf)));
+  const buffer = Buffer.concat(
+    [fromPublicKey.toBuffer(), Buffer.from(seed), programId.toBuffer()].map((buf) => new Uint8Array(buf)),
+  );
   const publicKeyBytes = sha256(new Uint8Array(buffer));
   return new PublicKey(publicKeyBytes);
 }
