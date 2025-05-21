2d8c56375df2c2b831b46de5d06a37043435335e
Ademola <83315082+0xcrust@users.noreply.github.com>
2025-05-21 13:21:17 +0100

    Export oracle calculator class

--- src/gfx/cpmm/index.ts
+++ src/gfx/cpmm/index.ts
@@ -4,4 +4,5 @@ export * from "./pda";
 export * from "./instruction";
 export * from "./curve/calculator";
 export * from "./curve/constantProduct";
+export * from "./curve/oracleCalculator"
 export * from "./curve/fee";
